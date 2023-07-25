c2bed8e43ab4f3a80bfa3dbaf0711447f327511f
Meghna <msrivastava@voltus.co>
2023-07-24 17:32:59 -0700

    use paths in app and navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/Home";
 import SixCoinPuzzle from "./pages/SixCoinPuzzle";
 import FiveCoinPuzzle from "./pages/FiveCoinPuzzle";
+import { PATHS } from "./paths";
 
 function App() {
-  // debugger;
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sixcoin" element={<SixCoinPuzzle />} />
-        <Route path="/fivecoin" element={<FiveCoinPuzzle />} />
+        <Route path={PATHS.HOME} element={<HomePage />} />
+        <Route path={PATHS.SIX_COIN} element={<SixCoinPuzzle />} />
+        <Route path={PATHS.FIVE_COIN} element={<FiveCoinPuzzle />} />
       </Routes>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles.css";
+import { PATHS } from "../paths";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <li>
-        <Link to="/">Home</Link>
+        <Link to={PATHS.HOME}>Home</Link>
       </li>
       <li>
-        <Link to="/sixcoin">Six Coin Puzzle</Link>
+        <Link to={PATHS.SIX_COIN}>Six Coin Puzzle</Link>
       </li>
       <li>
-        <Link to="/fivecoin">Five Coin Puzzle</Link>
+        <Link to={PATHS.FIVE_COIN}>Five Coin Puzzle</Link>
       </li>
     </div>
   );
